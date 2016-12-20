ad3862b549d9a515a537594a7bf47ab60a03cefb
Tsubasa <xxx.tubasa0226@gmail.com>
2016-12-20 11:53:07 +0900

    Rspec generate test for amp_image_tag_helper

--- spec/amp_helper_spec.rb
+++ spec/amp_helper_spec.rb
@@ -1,11 +1,32 @@
-require "spec_helper"
+require 'spec_helper'
+require 'amp_helper/amp_image_tag_helper'
+require 'fastimage'
+
+ActionView::Base.send(:include, AmpImageTagHelper)
 
 describe AmpHelper do
-  it "has a version number" do
+  it 'has a version number' do
     expect(AmpHelper::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+
+  describe 'amp_image_tag' do
+    before :all do
+      @image_url = 'http://placehold.it/350x150'
+      @view = ActionView::Base.new
+      @img_tag = '<amp-img alt="350x150" height="%d" src="http://placehold.it/'\
+                 '350x150" width="%d" /></amp-img>'
+    end
+
+    it 'url with dimensions returns amp-img tag' do
+      @view.amp_image_tag(@image_url, width: 20, height: 20).should(
+        eq(@img_tag % [20, 20].reverse)
+      )
+    end
+    it 'url without dimensions returns amp-img tag' do
+      @view.amp_image_tag(@image_url).should(
+        eq(@img_tag % [350, 150].reverse)
+      )
+    end
   end
 end
